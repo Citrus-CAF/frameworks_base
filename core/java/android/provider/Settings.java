7e88799f1a9bedf6ca16b5bd936fa0c9cd221262
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-04 02:17:27 +0000

    ADB over network [1/3]
    Includes:
    - ADB Over Network, integration of the adb-host mode (already present)
      The feature can be used/tested without the Settings part:
    
        setprop service.adb.tcp.port 5555
    
      Note: This ADB setting is not persistent (for security purpose) and
      require init.rc implementation event like this : http://bit.ly/AdbTcpIP
    
      Author: Tanguy Pruvot
      Id: I5c61a53948349c785356cb5aae165110d75e3074
    
      Author: sssemil <suleymanovemil8@gmail.com>
      Show notification on adb over network too
    
      Screenshots - http://goo.gl/TgsRI6
      Id: I9ddc0aa9a4f330a06ab5d97a8645d1b31bb6f299
    
      Author: Ethan Chen <intervigil@gmail.com>
      Use custom ADB over wifi property
      * Use a custom system property to trigger the real one, so we avoid
      running afoul of any SELinux CTS requirements.
    
    Change-Id: Ic8d4771e1ceee9a146739b42dc5f555c36333217
    
    Conflicts:
    	core/res/res/values/citrus_strings.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6546,6 +6546,18 @@ public final class Settings {
         public static final String DEVICE_PAIRED = "device_paired";
 
         /**
+         * Whether to display the ADB notification.
+         * @hide
+         */
+        public static final String ADB_NOTIFY = "adb_notify";
+
+        /**
+         * The TCP/IP port to run ADB on, or -1 for USB
+         * @hide
+         */
+        public static final String ADB_PORT = "adb_port";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
