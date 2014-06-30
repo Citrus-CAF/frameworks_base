76c5618bd34aca4f24f9b590472d96128d4a1305
Clark Scheff <clark@cyngn.com>
2014-06-30 13:39:37 -0700

    Themes: Update LoadedApk resources.
    Ported from cm-11.0 with lazy loading of resources
    
    Change-Id: I324eb5ebc106a531afc4b44ea5d32f272250f6a8

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -1845,8 +1845,7 @@ public final class ActivityThread {
             }
 
             LoadedApk packageInfo = ref != null ? ref.get() : null;
-            if (packageInfo == null || (packageInfo.mResources != null
-                    && !packageInfo.mResources.getAssets().isUpToDate())) {
+            if (packageInfo == null) {
                 if (localLOGV) Slog.v(TAG, (includeCode ? "Loading code package "
                         : "Loading resource-only package ") + aInfo.packageName
                         + " (in " + (mBoundApplication != null
@@ -1871,6 +1870,10 @@ public final class ActivityThread {
                     mResourcePackages.put(aInfo.packageName,
                             new WeakReference<LoadedApk>(packageInfo));
                 }
+            }
+            if (packageInfo.mResources != null
+                    && !packageInfo.mResources.getAssets().isUpToDate()) {
+                packageInfo.mResources = null;
             }
             return packageInfo;
         }
