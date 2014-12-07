c82c67955552b30977db7dfd5bfd97d5ad935de5
Kyrylo Mikos <kiril.mik.os@gmail.com>
2014-12-07 13:51:00 +0200

    frameworks/base: Don't report existing menu key if it was remapped. [2/3]
    Conflicts:
    	services/core/java/com/android/server/policy/PhoneWindowManager.java
    	services/core/java/com/android/server/wm/WindowManagerService.java
    
    Change-Id: Iabad06cd42cebecdff6f0440899432513681b063
    
    Conflicts:
    	services/core/java/com/android/server/wm/WindowManagerService.java

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -733,6 +733,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     private HardkeyActionHandler mKeyHandler;
 
+    private boolean mHasPermanentMenuKey;
+
     private class PolicyHandler extends Handler {
         @Override
         public void handleMessage(Message msg) {
@@ -786,11 +788,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 case HardkeyActionHandler.MSG_FIRE_HOME:
                     launchHomeFromHotKey();
                     break;
-//                case HardkeyActionHandler.MSG_UPDATE_MENU_KEY:
-//                    synchronized (mLock) {
-//                        mHasPermanentMenuKey = msg.arg1 == 1;
-//                    }
-//                    break;
+                case HardkeyActionHandler.MSG_UPDATE_MENU_KEY:
+                    synchronized (mLock) {
+                        mHasPermanentMenuKey = msg.arg1 == 1;
+                    }
+                    break;
                 case HardkeyActionHandler.MSG_DO_HAPTIC_FB:
                     performHapticFeedbackLw(null,
                             HapticFeedbackConstants.LONG_PRESS, false);
@@ -7554,6 +7556,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     // reflects factory or emulator prop override
     // basically a cm compatability shim
+    @Override
+    public boolean hasPermanentMenuKey() {
+        return !hasNavigationBar() && mHasPermanentMenuKey;
+    }
+
     public boolean needsNavigationBar() {
         return mHasNavigationBar;
     }
