e77d48e006e5d84f57d0455d309c07c11158b38f
Roman Birg <roman@cyngn.com>
2015-01-21 18:59:20 -0800

    Add NFC QS Tile
    More cleanup on QS tiles
    * Make strings & states more consistent to AOSP tiles
    * Remove unused/duplicate strings
    * Fix copyright headers
    Change-Id: Ib97f216c31c96e23d72ce7bf7e63b16b65f1bee0
    
    SystemUI: update quick tile animations and icons
    Use new AnimationIcons and ResourceIcon classes added with 5.1
    Change-Id: Ib904ac0cda2a2787f960893a3434cf21f392af3b
    
    Fix tile disapearing randomly
    
    PureNexus edits:
    *properly forward port to 7.0
    *add is available boolean hasSystemFeature(PackageManager.FEATURE_NFC)

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -57,7 +57,8 @@ import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.MusicTile;
 import com.android.systemui.qs.tiles.NavigationBarTile;
-import com.android.systemui.qs.tiles.NightDisplayTile; 
+import com.android.systemui.qs.tiles.NfcTile;
+import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
@@ -449,6 +450,8 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("reboot")) return new RebootTile(this);
         else if (tileSpec.equals("navigation_bar")) return new NavigationBarTile(this);
         else if (tileSpec.equals("heads_up")) return new HeadsUpTile(this);
+        else if (tileSpec.equals("nfc")) return new NfcTile(this);
+
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(this,tileSpec);
