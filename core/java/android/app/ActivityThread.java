3f475c20e7494dcef9035b3b429558f16040bc04
d34d <clark@cyngn.com>
2015-01-22 17:48:59 -0800

    Themes: Watch all app crashes for problematic themes
    Only watching for exceptions that occur during an apps launch may
    not be sufficient to catch possible bad themes causing issues.
    This patch monitors all app crashes and broadcasts that there was
    a crash if either Resources.NotFoundException or InflateException
    are thrown.  The remaining logic is the same as when an app launch
    failure occurred.
    
    We also display a notification to the user so that they know why
    their theme was reset to the system theme.
    
    Change-Id: I0761d02587b5b81deee4a31a89f515dbc7cc5fe6

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -2444,13 +2444,6 @@ public final class ActivityThread {
 
         } catch (Exception e) {
             if (!mInstrumentation.onException(activity, e)) {
-                if (e instanceof InflateException) {
-                    Log.e(TAG, "Failed to inflate", e);
-                    sendAppLaunchFailureBroadcast(r);
-                } else if (e instanceof Resources.NotFoundException) {
-                    Log.e(TAG, "Failed to find resource", e);
-                    sendAppLaunchFailureBroadcast(r);
-                }
                 throw new RuntimeException(
                     "Unable to start activity " + component
                     + ": " + e.toString(), e);
@@ -2465,7 +2458,7 @@ public final class ActivityThread {
         if (r.packageInfo != null && !TextUtils.isEmpty(r.packageInfo.getPackageName())) {
             pkg = r.packageInfo.getPackageName();
         }
-        Intent intent = new Intent(Intent.ACTION_APP_LAUNCH_FAILURE,
+        Intent intent = new Intent(Intent.ACTION_APP_FAILURE,
                 (pkg != null)? Uri.fromParts("package", pkg, null) : null);
         getSystemContext().sendBroadcast(intent);
     }
