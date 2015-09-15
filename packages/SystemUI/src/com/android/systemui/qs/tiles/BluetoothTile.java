dc7bc51ab68c9a8d45b91fa7716acf851105ce28
d34d <clark@cyngn.com>
2015-09-15 16:45:24 -0700

    QS: Make WiFi and Bluetooth tile detail view consistent
    Keep the detail view open when toggling on and off, just like the
    location tile.
    
    Change-Id: Ia19f28cf23e2c704b82099fa5edd03a09de91832
    TICKET: CYNGNOS-1023

--- packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
@@ -201,7 +201,7 @@ public class BluetoothTile extends QSTile<QSTile.BooleanState>  {
         public void setToggleState(boolean state) {
             MetricsLogger.action(mContext, MetricsLogger.QS_BLUETOOTH_TOGGLE, state);
             mController.setBluetoothEnabled(state);
-            showDetail(false);
+            fireToggleStateChanged(state);
         }
 
         @Override
