cacba1112fe29185542c726205164be24952f175
Puneet Mishra <puneetm@codeaurora.org>
2015-11-24 14:37:38 +0000

    frameworks/base: Support for third party NFC features
    Integration of below modifications are necessary to support
    third party NFC software:
    
    * a new interface in INfcAdapter.aidl allowing vendor specific
      extensions and features
    * a new size for MIFARE Classic tags
    * a modified constructor to distinguish MIFARE Classic tags
      from NfcA tags
    * allowing extensions to AidGroup and changing the protection
      of the instance variables to package protected
    
    Change-Id: Ic11dc68c4ea83262c705ec50b75b5808aa064f82
    (integrated from commit 57a001b7851c97d41f042dda643f9a87aa6306e5)

--- core/java/android/nfc/cardemulation/AidGroup.java
+++ core/java/android/nfc/cardemulation/AidGroup.java
@@ -4,6 +4,9 @@
  *
  * Copyright (C) 2015 The Android Open Source Project
  *
+ * Copyright (c) 2015, The Linux Foundation. All rights reserved.
+ * Not a Contribution.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
