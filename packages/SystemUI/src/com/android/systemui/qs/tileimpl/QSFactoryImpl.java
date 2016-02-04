cc71629e5625033b3b7ef0bc9bca31bc6e8842a9
Keith Mok <kmok@cyngn.com>
2016-02-04 20:10:00 +0100

    SystemUI: Add Caffeine QS tile
    *DU Edits*
    - Adopt to P style tiles and utilize just one icon.
    - Removed unused imports and strings
    
    * Citrus-CAF edits:
      Add contentDescription
    
    Change-Id: I03bfc8c9bae4a4be92224c1eccbae0d58b0b7769

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -28,6 +28,7 @@ import com.android.systemui.qs.tiles.AirplaneModeTile;
 import com.android.systemui.qs.tiles.ANBITile;
 import com.android.systemui.qs.tiles.BatterySaverTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
+import com.android.systemui.qs.tiles.CaffeineTile;
 import com.android.systemui.qs.tiles.CastTile;
 import com.android.systemui.qs.tiles.CellularTile;
 import com.android.systemui.qs.tiles.ColorInversionTile;
@@ -126,6 +127,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new ANBITile(mHost);
             case "navigation_bar":
                 return new NavigationBarTile(mHost);
+            case "caffeine":
+                return new CaffeineTile(mHost);
         }
 
         // Intent tiles.
