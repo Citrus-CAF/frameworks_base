0119cef84d7fbc0c964903f5810d5b3b3aa4af20
dankoman <dankoman30@gmail.com>
2016-02-06 14:56:54 -0600

    Fw_base - let InCallUI handle proximity sensor for incoming calls [1/2]
    fixes "proximity sensor doesn't control screen state on incoming
            call.  must manually turn on screen to see who's calling."
    
    Conflicts:
    	services/core/java/com/android/server/power/PowerManagerService.java

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -194,6 +194,8 @@ public final class PowerManagerService extends SystemService
     private int mButtonBrightness;
     private int mButtonBrightnessSettingDefault;
 
+    private TelephonyManager mTelephonyManager;
+
     private final Object mLock = new Object();
 
     // A bitfield that indicates what parts of the power state have
@@ -3346,6 +3348,14 @@ public final class PowerManagerService extends SystemService
             } else {
                 r.run();
             }
+        }
+        
+        TelephonyManager getTelephonyManager() {
+            if (mTelephonyManager == null) {
+                mTelephonyManager = (TelephonyManager)mContext.getSystemService(
+                        Context.TELEPHONY_SERVICE);
+            }
+            return mTelephonyManager;
         }
 
         private void runPostProximityCheck(final Runnable r) {
