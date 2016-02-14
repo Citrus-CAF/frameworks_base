f818860bd52dcd9818a77cbe1e9386d6a9513041
beanstown106 <nbenis106@gmail.com>
2016-02-14 15:53:02 -0500

    Fix Dual tiles in qs tuner
    - Previously having show main tiles on showed 3/4/5 tiles on the top row
    and didnt reflect what was actually shown in the quick settings panel this
    was resolved by removing main tiles from the observer, also added a call
    to refresh all tiles on drop and on tiles changed as without these the top
    main tiles would loose their main tiles look in the tuner after a tile was
    changed, also remove all references to supportsDualTargets
    
    Change-Id: Ifd5e483360fb37f517cb8199cd0a6306c98883b7

--- packages/SystemUI/src/com/android/systemui/qs/QSTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSTile.java
@@ -86,10 +86,6 @@ public abstract class QSTile<TState extends State> implements Listenable {
         mHandler = new H(host.getLooper());
     }
 
-    public boolean supportsDualTargets() {
-        return false;
-    }
-
     public Host getHost() {
         return mHost;
     }
