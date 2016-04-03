dcf5504a3c4d29bd95a51e2fb4971f3400605b2e
htchoi1995 <htchoi1995@gmail.com>
2016-04-03 09:24:26 -0400

    [1/2] SystemUI: Network speed indicator
    Conflicts:
    	packages/SystemUI/res/layout/status_bar.xml
    	packages/SystemUI/res/values/colors.xml
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3629,6 +3629,12 @@ public final class Settings {
          */
         public static final String LOCK_SCREEN_SHOW_CURRENT = "lock_screen_show_current";
 
+
+        /* Network speed indicator
+         * @hide
+         */
+        public static final String STATUS_BAR_TRAFFIC = "status_bar_traffic";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
