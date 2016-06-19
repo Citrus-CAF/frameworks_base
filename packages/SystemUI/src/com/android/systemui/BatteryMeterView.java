83f2f3b31ada89f23f26e50780d0acffeeef3dfb
Willi Ye <williye97@gmail.com>
2016-06-20 01:26:09 +0200

    BatteryMeterView: Pulse 3 times when plugging in the charger
    Change-Id: Ic1917b8f928613633293a5cf543d2369c763bee9

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -460,7 +460,7 @@ public class BatteryMeterView extends View implements DemoMode,
         void setDarkIntensity(int backgroundColor, int fillColor);
     }
 
-    protected class AllInOneBatteryMeterDrawable  implements BatteryMeterDrawable {
+    protected class AllInOneBatteryMeterDrawable implements BatteryMeterDrawable {
         private static final boolean SINGLE_DIGIT_PERCENT = false;
         private static final boolean SHOW_100_PERCENT = false;
 
@@ -547,7 +547,7 @@ public class BatteryMeterView extends View implements DemoMode,
             }
             if (mChargingAnimationsEnabled && !mThemeApplied) {
                 if (tracker.level < 100 && tracker.plugged) {
-                    startChargingAnimation(true);
+                    startChargingAnimation(0);
                 } else {
                     cancelChargingAnimation();
                 }
@@ -556,16 +556,17 @@ public class BatteryMeterView extends View implements DemoMode,
 
         @Override
         public void onBatteryLevelChanged(int level, boolean pluggedIn, boolean charging) {
-            if (charging && !mThemeApplied && !mChargingAnimationsEnabled
+            if (pluggedIn && !mThemeApplied && !mChargingAnimationsEnabled
                     && mLevel != level) {
-                startChargingAnimation(false);
+                startChargingAnimation(mLevel == 0 ? 3 : 1);
                 mLevel = level;
-            } else {
+            } else if (!pluggedIn) {
                 mLevel = 0;
+                cancelChargingAnimation();
             }
         }
 
-        private void startChargingAnimation(final boolean invalidate) {
+        private void startChargingAnimation(final int repeat) {
             if (mLevelAlpha == 0 || mAnimator != null
                     || mMeterMode != BatteryMeterMode.BATTERY_METER_CIRCLE) {
                 return;
@@ -581,25 +582,30 @@ public class BatteryMeterView extends View implements DemoMode,
                 }
             });
             mAnimator.addListener(new AnimatorListenerAdapter() {
+                private boolean mCanceled;
+
                 @Override
                 public void onAnimationCancel(Animator animation) {
+                    mCanceled = true;
                     mLevelDrawable.setAlpha(defaultAlpha);
                     mAnimator = null;
+                    invalidate();
                 }
 
                 @Override
                 public void onAnimationEnd(Animator animation) {
+                    if (mCanceled) return;
                     mLevelDrawable.setAlpha(defaultAlpha);
                     mAnimator = null;
-                    if (invalidate) {
-                        invalidate();
+                    if (repeat <= 0) {
+                        startChargingAnimation(0);
+                    } else if (repeat != 1) {
+                        startChargingAnimation(repeat - 1);
                     }
                 }
             });
             mAnimator.setDuration(2000);
-            if (invalidate) {
-                mAnimator.setStartDelay(500);
-            }
+            mAnimator.setStartDelay(500);
             mAnimator.start();
         }
 
