8238855f4a905aeb460533ec604456738887a3c7
Adarsh-MR <adarshmr1998@gmail.com>
2016-07-02 20:27:57 +0000

    Revert "SystemUI: Add Rebooting Progress Dialog string"
    This reverts commit 36af0f8c6355ec10d06b183978b62a73554aeb80.

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -298,13 +298,8 @@ public final class ShutdownThread extends Thread {
                 pd.setIndeterminate(true);
             }
         } else {
-            if (mReboot) {
-                pd.setTitle(context.getText(com.android.internal.R.string.reboot_title));
-                pd.setMessage(context.getText(com.android.internal.R.string.reboot_progress));
-            } else {
-                pd.setTitle(context.getText(com.android.internal.R.string.power_off));
-                pd.setMessage(context.getText(com.android.internal.R.string.shutdown_progress));
-            }
+            pd.setTitle(context.getText(com.android.internal.R.string.power_off));
+            pd.setMessage(context.getText(com.android.internal.R.string.shutdown_progress));
             pd.setIndeterminate(true);
         }
         pd.setCancelable(false);
