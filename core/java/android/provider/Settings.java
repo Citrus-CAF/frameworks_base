d84794d23d4e4e810ff6347be5e1b640216d2e8d
Corey Edwards <ensabahnur16@gmail.com>
2016-08-09 09:07:17 -0500

    Enable\Disable bluetooth battery status
    Based on this patch:
    http://review.cyanogenmod.org/#/c/153034/
    https://github.com/CyanogenMod/android_frameworks_base/commit/88e7a6c7fcaf71d6dae2b9067db19aaeba7de39b
    
    This just makes it configurable for the user, in case they don't want to see that, or if their themes doesn't support the new icons yet.
    
    nathanchance edits: combined the two patches from Corey into one due to the SystemUI tuner fragment
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Change-Id: If8709512dfd39bf797338d3d6f9630e7fdb47acf

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4123,6 +4123,12 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_ANYWHERE = "double_tap_sleep_anywhere";
 
         /**
+         * Enable\Disable Bluetooth Battery bar
+         * @hide
+         */
+        public static final String BLUETOOTH_SHOW_BATTERY = "bluetooth_show_battery";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
