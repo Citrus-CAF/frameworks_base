0d251be982acb4cc886d420f7586fd7ab2067138
mydongistiny <jaysonedson@gmail.com>
2016-09-15 19:27:55 -0700

    QS: WiFi easy toggle [1/2]
    Change-Id: Icb8f8d5f965385be218a6dab06774e3adae3750d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6817,6 +6817,13 @@ public final class Settings {
          */
         public static final String LOCKSCREEN_VISUALIZER_ENABLED = "lockscreen_visualizer";
 
+
+        /**
+         * Enable single click to turn WiFi on or off.
+         * @hide
+         */
+        public static final String QS_WIFI_EASY_TOGGLE = "qs_wifi_easy_toggle";
+
         /**
          * Whether the device should doze if configured.
          * @hide
