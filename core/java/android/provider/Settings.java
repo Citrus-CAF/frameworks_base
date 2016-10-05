2ced208f6e66b3c3a52790de35d8b1a47c1e9822
xyyx <xyyx@mail.ru>
2016-10-05 09:06:44 +0800

    Bring back 'custom small QS tiles' [1/2]
    Change-Id: I0d5829643ab37ea76863c9efeeff64b9e2961f58

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7214,6 +7214,12 @@ public final class Settings {
         public static final String ADB_PORT = "adb_port";
 
         /**
+         * Count of small QS tiles.
+         * @hide
+         */
+        public static final String QQS_COUNT = "sysui_qqs_count";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
