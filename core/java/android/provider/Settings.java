baeff16715e9635549dfd79c590bc6999b5fd743
beanstown106 <nbenis106@gmail.com>
2016-10-06 21:10:33 +0200

    Fingerprint authentication vibration [1/2]
    had a few requests for this, as most devices only vibrate when fingerprint authentication is unsucesful so made it configurable
    
    Change-Id: I7fc6f7fff7002b39645996bc6791e3a06d501247

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3989,6 +3989,12 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_ANYWHERE = "double_tap_sleep_anywhere";
 
         /**
+         * whether to enable or disable vibration on succesful fingerprint auth
+         * @hide
+         */
+        public static final String FINGERPRINT_SUCCESS_VIB = "fingerprint_success_vib";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
