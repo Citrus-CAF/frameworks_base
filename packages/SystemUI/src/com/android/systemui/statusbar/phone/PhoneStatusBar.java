c4e76491f23e4623c8138bc0d98032b20b027a38
maxwen <max.weninger@gmail.com>
2016-10-07 14:44:56 +0200

    base: allow disabling quick settings on secure lock screens [1/2]
    inspired by
    SlimRoms/frameworks_base@b76f8af
    DirtyUnicorns/android_frameworks_base@3a4d3be
    
    Also hide keyguard shortcuts cause via these
    you could still access quick settings
    
    Change-Id: Ia048f9ecf3eed56d3aec40eb2bbe1632d1b14148

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -537,6 +537,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
            resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.QS_LAYOUT_COLUMNS), false, this,
                     UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.LOCK_QS_DISABLED), false, this,
+                    UserHandle.USER_ALL);
             update();
        }
 
@@ -581,6 +584,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
             mQsLayoutColumns = Settings.System.getIntForUser(resolver,
                     Settings.System.QS_LAYOUT_COLUMNS, 3, mCurrentUserId);
+
+            if (mNotificationPanel != null) {
+                mNotificationPanel.update();
+            }
 
             if (mHeader != null) {
                 mHeader.update();
