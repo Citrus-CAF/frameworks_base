52c5b7446253e5c54cf6e8d61736b6c34451a119
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-10-20 00:08:00 +0000

    [1/2] Frameworks_base: Double tap to wake on doze
    Thanx to Patrick for this excellent idea and to Dankoman for pointing me in the right direction ;)
    
    PS2: Fix dt2s : ignore touch events for dt2s while dozing
    
    PS3: fix "swipe down turns display off and disables doze until screen is turned on at least once"
    
    PS4: disable camera and mic icons when dozing
    
    YAOSP : port to N
    AICP: Add the contentobserver from 991a0555d80bee624e4b93302823b64d8e57c93a
    
    Change-Id: I36162b60895f0b9e98132479edd4e74199662e29
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowView.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4409,6 +4409,12 @@ public final class Settings {
          */
         public static final String ALLOW_INCALL_HOME = "allow_incall_home";
 
+       /**
+         * Require double tap instead of simple tap to wake from Doze pulse screen
+         * @hide
+         */
+        public static final String DOUBLE_TAP_WAKE_DOZE = "double_tap_wake_doze";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
