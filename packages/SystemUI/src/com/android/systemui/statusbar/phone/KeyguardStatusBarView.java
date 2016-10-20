1b03292792ff8ef6fc650f8f2de7c953c07e6925
LorDClockaN <lordclockan@gmail.com>
2016-10-20 22:31:22 +0200

    Set carrier label on keyguard to empty string if isWifiOnly device
    Change-Id: Iae0f8bcb94c4065c669a942180d03b3bb247d7e0

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -33,6 +33,7 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.android.internal.util.custom.Customtils;
 import com.android.systemui.BatteryMeterView;
 import com.android.systemui.Interpolators;
 import com.android.systemui.R;
@@ -97,6 +98,9 @@ public class KeyguardStatusBarView extends RelativeLayout
         mMultiUserAvatar = (ImageView) findViewById(R.id.multi_user_avatar);
         //mBatteryLevel = (TextView) findViewById(R.id.battery_level);
         mCarrierLabel = (TextView) findViewById(R.id.keyguard_carrier_text);
+        if (CustomUtils.isWifiOnly(getContext())) {
+            mCarrierLabel.setText("");
+        }
         loadDimens();
         updateUserSwitcher();
         updateVisibilities();
