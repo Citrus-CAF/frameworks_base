8ae6e6db8be310aa758c6b31bbbc6753b6bf6b02
beanstown106 <nbenis106@gmail.com>
2016-11-10 22:12:35 -0500

    Advanced screenshot functionality [1/2] *shortened the screenshot timeout on screenshot power menu entry and qs tile *longpress power button screenshot item for selectable screenshot *longpress quick settings button for selectable screenshot *ability to force selectable screenshots on volume up and power instead of screen record
    Credits:
    @mazda-- for finding the selectable screenshot method
    @tsubus for the idea for adding screenrecord chord type

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -743,6 +743,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     // use the same delay values as for screenshot
     private boolean mScreenrecordChordEnabled;
     private boolean mScreenrecordChordVolumeUpKeyConsumed;
+    private boolean mScreenrecordChordType;
 
     /* The number of steps between min and max brightness */
     private static final int BRIGHTNESS_STEPS = 10;
@@ -981,6 +982,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.KEYGUARD_TOGGLE_TORCH), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SCREENRECORD_CHORD_TYPE), false, this,
                     UserHandle.USER_ALL);
             updateSettings();
         }
@@ -1515,7 +1519,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 mScreenrecordChordVolumeUpKeyConsumed = true;
                 cancelPendingPowerKeyAction();
 
-                mHandler.postDelayed(mScreenrecordRunnable, getScreenshotChordLongPressDelay());
+                if (mScreenrecordChordType) {
+                    mScreenshotRunnable.setScreenshotType(TAKE_SCREENSHOT_SELECTED_REGION);
+                    mHandler.postDelayed(mScreenshotRunnable, getScreenshotChordLongPressDelay());
+                } else {
+                    mHandler.postDelayed(mScreenrecordRunnable, getScreenshotChordLongPressDelay());
+                }
             }
         }
     }
@@ -2253,6 +2262,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
             mTorchEnabled = (Settings.System.getIntForUser(resolver,
                     Settings.System.KEYGUARD_TOGGLE_TORCH, 0, UserHandle.USER_CURRENT) == 1);
+
+            mScreenrecordChordType = (Settings.System.getIntForUser(resolver,
+                    Settings.System.SCREENRECORD_CHORD_TYPE, 0, UserHandle.USER_CURRENT) == 1);
         }
         synchronized (mWindowManagerFuncs.getWindowManagerLock()) {
             WindowManagerPolicyControl.reloadFromSetting(mContext);
