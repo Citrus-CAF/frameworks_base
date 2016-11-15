424289ae1a073cbf434022cec54964c845250b3e
tsubus <tsubus@ilwt.org>
2016-11-15 11:44:42 +0100

    Always show battery percentage when charging
    - also fix a bug from upstream where battery percentage
      wouldn't show on lockscreen with battery style
      set to 'Text'
    
    Change-Id: I3965c1d6a0828bf079cbb495b5f2601a9c0a9aa6

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -75,6 +75,7 @@ public class KeyguardStatusBarView extends RelativeLayout
     private View mSystemIconsContainer;
 
     private boolean mShowBatteryText;
+    private boolean mForceBatteryText;
 
     private ContentObserver mObserver = new ContentObserver(new Handler()) {
         public void onChange(boolean selfChange, Uri uri) {
@@ -183,7 +184,7 @@ public class KeyguardStatusBarView extends RelativeLayout
         }
 
         mBatteryLevel.setVisibility(
-                mBatteryCharging || mShowBatteryText ? View.VISIBLE : View.GONE);
+                mBatteryCharging || mShowBatteryText || mForceBatteryText ? View.VISIBLE : View.GONE);
 
         mBatteryLevel.setBatteryCharging(mBatteryCharging);
         if (mCarrierLabel != null) {
@@ -371,6 +372,8 @@ public class KeyguardStatusBarView extends RelativeLayout
     public void onTuningChanged(String key, String newValue) {
         if (key.equals(STATUS_BAR_SHOW_BATTERY_PERCENT)) {
             mShowBatteryText = newValue == null ? false : Integer.parseInt(newValue) == 2;
+            mForceBatteryText = Settings.Secure.getInt(getContext().getContentResolver(),
+                    Settings.Secure.STATUS_BAR_BATTERY_STYLE, 0) == 6 ? true : false;
             updateVisibilities();
         }
     }
