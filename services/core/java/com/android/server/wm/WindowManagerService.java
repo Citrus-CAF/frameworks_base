a226402d1b884e74aa18f7e442c3cafa01d8e182
Ivaylo Asenov <ivaylo.asenov@linaro.org>
2017-01-10 13:33:25 +0200

    Stop bootanim earlier.
    Change-Id: I6344474f530b6cb7584892063ab6c83f871716c7
    Signed-off-by: Ivaylo Asenov <ivaylo.asenov@linaro.org>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -6064,10 +6064,6 @@ public class WindowManagerService extends IWindowManager.Stub
                 return;
             }
 
-            // Don't enable the screen until all existing windows have been drawn.
-            if (!mForceDisplayEnabled && checkWaitingForWindowsLocked()) {
-                return;
-            }
 
             if (!mBootAnimationStopped) {
                 // Do this one time.
@@ -6088,7 +6084,13 @@ public class WindowManagerService extends IWindowManager.Stub
                 mBootAnimationStopped = true;
             }
 
-            if (!mForceDisplayEnabled && !checkBootAnimationCompleteLocked()) {
+
+	   // Don't enable the screen until all existing windows have been drawn.
+            if (!mForceDisplayEnabled && checkWaitingForWindowsLocked()) {
+                return;
+            }
+
+	   if (!mForceDisplayEnabled && !checkBootAnimationCompleteLocked()) {
                 if (DEBUG_BOOT) Slog.i(TAG_WM, "performEnableScreen: Waiting for anim complete");
                 return;
             }
