03285d752218971f6f1b3d993a14fb4fa2221f55
Dave Kessler <activethrasher00@gmail.com>
2017-01-17 04:28:18 +0100

    Add membar to recents options [1/2]
    Change-Id: Id1e799729543e385767ec8b68b126a20c4874baf
    
    Fix position without search bar
    
    In portrait and fix wrong bottom position in landscape with serch bar on.
    Also centralize text
    
    Change-Id: I544f75562229c921b491618c21397a42abef825d
    
    Simplify getting total memory
    
    Change-Id: I7cf089402f21b5bc72196b0dabdbb4963037ad5b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4017,6 +4017,12 @@ public final class Settings {
          * @hide
          */
         public static final String  RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+ 
+        /**
+         * show the membar in recents
+         *  @hide
+         */
+        public static final String SYSTEMUI_RECENTS_MEM_DISPLAY = "systemui_recents_mem_display";
 
         /**
          * Wether to show the ticker on the status bar
