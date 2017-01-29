5a7b4d83a44e7bfd7e0bb6fc0e941ef5d12e04e7
Adarsh-MR <rohitp4all@gmail.com>
2017-01-29 16:27:04 +0000

    Revert "Blur: Implement Recents Blur (1/2)"
    This reverts commit 66299169f88c7d7110ac1acf7e3f38f5766ef851.

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -1276,7 +1276,6 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     @Override
     public void toggleRecentApps() {
-        RecentsActivity.startBlurTask();
         toggleRecents();
     }
 
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -95,7 +95,6 @@ import android.os.UserHandle;
 import android.os.UserManager;
 import android.os.Vibrator;
 import android.provider.Settings;
-import com.android.systemui.recents.RecentsActivity;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.NotificationListenerService.RankingMap;
 import android.service.notification.StatusBarNotification;
@@ -428,9 +427,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private boolean mTranslucentNotifications;
     private int mQSTranslucencyPercentage;
     private int mNotTranslucencyPercentage;
-    private boolean mBlurredRecents;
-    private int mRadiusRecents;
-    private int mScaleRecents;
 
     // RemoteInputView to be activated after unlock
     private View mPendingRemoteInputView;
@@ -601,12 +597,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     Settings.System.TRANSLUCENT_QUICK_SETTINGS_PRECENTAGE_PREFERENCE_KEY), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.TRANSLUCENT_NOTIFICATIONS_PRECENTAGE_PREFERENCE_KEY), false, this, UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.RECENT_APPS_ENABLED_PREFERENCE_KEY), false, this, UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.RECENT_APPS_SCALE_PREFERENCE_KEY), false, this);
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.RECENT_APPS_RADIUS_PREFERENCE_KEY), false, this);
             update();
        }
 
@@ -675,15 +665,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     Settings.System.TRANSLUCENT_QUICK_SETTINGS_PRECENTAGE_PREFERENCE_KEY, 60);
             mNotTranslucencyPercentage = Settings.System.getInt(mContext.getContentResolver(),
                     Settings.System.TRANSLUCENT_NOTIFICATIONS_PRECENTAGE_PREFERENCE_KEY, 70);
-            mBlurredRecents = Settings.System.getIntForUser(resolver,
-                    Settings.System.RECENT_APPS_ENABLED_PREFERENCE_KEY, 0, UserHandle.USER_CURRENT) == 1;
-            mScaleRecents = Settings.System.getInt(mContext.getContentResolver(),
-                    Settings.System.RECENT_APPS_SCALE_PREFERENCE_KEY, 6);
-            mRadiusRecents = Settings.System.getInt(mContext.getContentResolver(),
-                    Settings.System.RECENT_APPS_RADIUS_PREFERENCE_KEY, 3);
-                    
-            RecentsActivity.updateRadiusScale(mScaleRecents,mRadiusRecents);
-            
+            //updatePreferences(this.mContext);
+            //RecentsActivity.startBlurTask();
+
             mQsLayoutColumns = Settings.System.getIntForUser(resolver,
                     Settings.System.QS_LAYOUT_COLUMNS, 3, mCurrentUserId);
 
@@ -1378,13 +1362,14 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     if (NotificationPanelView.mKeyguardShowing) {
                         return;
                     }
+                    //RecentsActivity.onConfigurationChanged();
                     String action = intent.getAction();
 
                if (action.equals(Intent.ACTION_CONFIGURATION_CHANGED)) {
                         if (NotificationPanelView.mKeyguardShowing) {
                             return;
                         }
-                        RecentsActivity.onConfigurationChanged();
+                        //RecentsActivity.onConfigurationChanged();
 
                         if (mExpandedVisible && NotificationPanelView.mBlurredStatusBarExpandedEnabled && (!NotificationPanelView.mKeyguardShowing)) {
                             makeExpandedInvisible();
@@ -1397,7 +1382,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             intent.addAction(Intent.ACTION_CONFIGURATION_CHANGED);
             this.mContext.registerReceiver(receiver, intent);
 
-            RecentsActivity.init(this.mContext);
+            //RecentsActivity.init(this.mContext);
 
             updatePreferences(this.mContext);
         } catch (Exception e){
@@ -1426,7 +1411,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     }
 
     public static void updatePreferences(Context context) {
-        RecentsActivity.updatePreferences(context);
         BaseStatusBar.updatePreferences();
     }
     
