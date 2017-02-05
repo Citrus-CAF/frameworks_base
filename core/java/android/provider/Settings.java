52da24e8c84d18174e055129df0be71c1b51bae7
xyyx <xyyx@mail.ru>
2017-02-05 14:31:26 +0800

    Unlock keystore with fingerprint after reboot (1/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4289,6 +4289,12 @@ public final class Settings {
          public static final String LOCKSCREEN_MEDIA_METADATA = "lockscreen_media_metadata";
 
         /**
+         * Unlock keystore with fingerprint after reboot
+         * @hide
+         */
+        public static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
