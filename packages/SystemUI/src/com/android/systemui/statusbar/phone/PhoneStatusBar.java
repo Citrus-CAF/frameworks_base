6df0defb75e8f0b6a14184d7fcdb795b7a7faf01
Adarsh-MR <adarshmr1998@gmail.com>
2017-02-11 15:15:01 +0000

    fix derp
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1316,7 +1316,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     mStatusBarHeaderMachine.updateEnablement();
 
                     // Update all other settings
-                    mHeader.updateSettings();
+                    mHeader.update();
 
                     initSignalCluster(mHeader);
                     mHeader.setActivityStarter(PhoneStatusBar.this);
