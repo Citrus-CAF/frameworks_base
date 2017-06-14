319c3db09082818a9845fd1a82255b53b5f04a12
Surge1223 <surge1223@gmail.com>
2017-06-14 08:28:06 +0000

    BASE: Rootless restart SystemUI method
    Original commit: https://github.com/VelvetProject/packages_apps_ThemeInterfacer/commit/3484db3dda1e7bf7e3f6fda049b903968f1506e7)
    
    AICPfy:
    - Clean up unrequired code from previous implementation: https://github.com/AICP/frameworks_base/commit/d4d72acdc931d24a620911dbf5527c45244310c0
    - In fw_base, we can use the classes directly

--- core/java/com/android/internal/util/custom/CustomUtils.java
+++ core/java/com/android/internal/util/custom/CustomUtils.java
@@ -16,7 +16,13 @@
 
 package com.android.internal.util.custom;
 
+
+import android.app.ActivityManager;
+import android.app.ActivityManagerNative;
+import android.app.IActivityManager;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -32,6 +38,9 @@ import android.view.WindowManager;
 import java.util.Locale;
 
 public class CustomUtils {
+
+    // avoids hardcoding the tag
+    private static final String TAG = Thread.currentThread().getStackTrace()[1].getClassName();
 
     private static int sDeviceType = -1;
     private static final int DEVICE_PHONE = 0;
@@ -135,5 +144,22 @@ public class CustomUtils {
 
     public static boolean isTablet(Context context) {
         return getScreenType(context) == DEVICE_TABLET;
+    }
+    
+    public static void restartSystemUI(Context context) {
+        try {
+            ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+            IActivityManager amn = ActivityManagerNative.getDefault();
+            context.stopService(new Intent().setComponent(new ComponentName("com.android.systemui", "com.android.systemui.SystemUIService")));
+            am.killBackgroundProcesses("com.android.systemui");
+            for (ActivityManager.RunningAppProcessInfo app : am.getRunningAppProcesses()) {
+                if ("com.android.systemui".equals(app.processName)) {
+                    amn.killApplicationProcess(app.processName, app.uid);
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
