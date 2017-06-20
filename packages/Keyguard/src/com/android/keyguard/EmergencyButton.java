90003b1d895334965a8445eabe9cedf8f407ab71
Adarsh-MR <adarshmr1998@gmail.com>
2017-06-20 12:40:46 +0000

    Revert "Emergency call button on Swipe lock Screen."
    This reverts commit e7b6415c010c269e34cce09277148f7b2f91bcf5.

--- packages/Keyguard/src/com/android/keyguard/EmergencyButton.java
+++ packages/Keyguard/src/com/android/keyguard/EmergencyButton.java
@@ -195,7 +195,7 @@ public class EmergencyButton extends Button {
         }
     }
 
-    public void updateEmergencyCallButton() {
+    private void updateEmergencyCallButton() {
         boolean visible = false;
         if (mIsVoiceCapable) {
             // Emergency calling requires voice capability.
@@ -209,8 +209,7 @@ public class EmergencyButton extends Button {
                     visible = mEnableEmergencyCallWhileSimLocked;
                 } else {
                     // Only show if there is a secure screen (pin/pattern/SIM pin/SIM puk);
-                    visible = mLockPatternUtils.isSecure(KeyguardUpdateMonitor.getCurrentUser()) ||
-                            SystemProperties.getBoolean("persist.radio.emgcy_btn_onswipe", false);
+                    visible = mLockPatternUtils.isSecure(KeyguardUpdateMonitor.getCurrentUser());
                 }
             }
         }
