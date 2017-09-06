f8466a1fa27dbd136828e23a129df29d87ffa49c
fusionjack <dogfight60-fusionjack@yahoo.de>
2017-09-06 09:12:37 +0800

    base: Quick Settings: Add Reboot/Recovery tile
    * NitrogenOS edits:: Adapted and reworked to O
    
    * Citrus-CAF edits: Change "Reboot" string to "Restart" to match power menu.

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -38,6 +38,7 @@ import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
+import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WifiTile;
@@ -102,6 +103,8 @@ public class QSFactoryImpl implements QSFactory {
             // Custom tiles.
             case "adb_network":
                 return new AdbOverNetworkTile(mHost);
+            case "reboot":
+                return new RebootTile(mHost);
         }
 
         // Intent tiles.
