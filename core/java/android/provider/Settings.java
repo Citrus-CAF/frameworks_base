f3bef2d00534a9acd6bc4d1f7fe95a9a34b073b1
maxwen <max.weninger@gmail.com>
2018-02-07 10:13:26 +0000

    base: add placeholder setting for device features
    setting slot for devices to store settings that are device
    specific
    
    Change-Id: Id9f0be9ee6e6a02b7ea7bd70c7497482b053446d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4475,6 +4475,25 @@ public final class Settings {
         public static final String HIDE_FROM_RECENTS_LIST = "hide_from_recents_list";
 
         /**
+         * Enable proxi check for wake keys - must be implemented in a device
+         * KeyHandler
+         * @hide
+         */
+        public static final String DEVICE_PROXI_CHECK_ENABLED = "device_proxi_check_enabled";
+
+        /**
+         * @hide
+         */
+        public static final String DEVICE_FEATURE_SETTINGS = "device_feature_settings";
+
+        /** some devices have a extra hw button e.g. n3 on the back on the
+         * fingerprint sensor. allow mapping button to key
+         *
+         * @hide
+         */
+        public static final String BUTTON_EXTRA_KEY_MAPPING = "button_extra_mapping";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4698,7 +4717,10 @@ public final class Settings {
             PRIVATE_SETTINGS.add(RECENTS_ICON_PACK);
             PRIVATE_SETTINGS.add(STATUS_BAR_FILE_HEADER_IMAGE);
             PRIVATE_SETTINGS.add(HIDE_FROM_RECENTS_LIST);
-
+            PRIVATE_SETTINGS.add(DEVICE_PROXI_CHECK_ENABLED);
+            PRIVATE_SETTINGS.add(DEVICE_FEATURE_SETTINGS);
+            PRIVATE_SETTINGS.add(BUTTON_EXTRA_KEY_MAPPING);
+            
         }
         
         /**
