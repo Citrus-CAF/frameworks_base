c75476fdd04b00ac0e5bfa522561a66866289810
Ethan Chen <intervigil@gmail.com>
2018-03-08 12:23:32 -0800

    PackageManager: Catch possible NPE from unparsed platform package
    Change-Id: I5e1623aec670b54ca2206561d6177f5173f2cd12

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -9337,6 +9337,8 @@ public class PackageManagerService extends IPackageManager.Stub
             PackageParser.collectCertificates(pkg, policyFlags);
         } catch (PackageParserException e) {
             throw PackageManagerException.from(e);
+        } catch (NullPointerException e) {
+            throw new PackageManagerException(e.getMessage());
         } finally {
             Trace.traceEnd(TRACE_TAG_PACKAGE_MANAGER);
         }
